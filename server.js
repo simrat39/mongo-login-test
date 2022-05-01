a36b52ad74045731da784a34322cda6290ba687c
Simrat Grewal <simrats169169@gmail.com>
2022-04-30 18:58:28 -0700

    feat: Add basic html and serve it

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const fs = require("fs");
 
 async function main() {
   mongoose.connect("mongodb://127.0.0.1:27017/tutorria");
+
+  app.get("/", async (_, res) => {
+    const doc = fs.readFileSync("./public/html/landing.html", "utf-8");
+    res.send(doc);
+  });
 }
 
 app.listen(8000, main);
